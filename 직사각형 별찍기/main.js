dc98f7cdabc25f86344e91c174c39b2cc8f892e0
방재호 <awesomelon@naver.com>
2020-11-10 01:04:58 +0900

    update

--- 직사각형 별찍기/main.js
+++ 직사각형 별찍기/main.js
@@ -3,8 +3,7 @@ process.stdin.on('data', (data) => {
   const n = data.split(' ');
   const a = Number(n[0]),
     b = Number(n[1]);
-  const stars = '*'.repeat(a * b);
-  const reg = new RegExp('(.{' + a + '})', 'g');
-  const result = stars.replace(reg, '$1\n');
-  console.log(result);
+  const row = `${'*'.repeat(a)}\n`;
+  const col = row.repeat(b);
+  console.log(col);
 });
